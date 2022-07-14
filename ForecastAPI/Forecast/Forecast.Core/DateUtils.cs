f17c6bd15f2a7eda425124b4d0576d592f8e1c29
Janani1403 <janani.haran1403@gmail.com>
2022-07-14 16:04:55 +0530

    Add files via upload

---
+++ ForecastAPI/Forecast/Forecast.Core/DateUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Forecast.Core.Utils
+{
+    /// <summary>
+    /// Custom util to retrieve timestamp
+    /// </summary>
+    public static class DateUtils
+    {
+        public static long GetUtcTimestamp(DateTime date) {
+            var updatedDate = new DateTime(date.Year, date.Month, date.Day, 23, 59, 59);
+            return (long)(updatedDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+    }
+}
---
+++ ForecastAPI/Forecast/Forecast.Core/JsonPathConverter.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Forecast.Core.Utils
+{
+    /// <summary>
+    /// Custom util to map JSON nested objects
+    /// </summary>
+    public class JsonPathConverter : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType,
+                                    object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                JObject sourceObject = JObject.Load(reader);
+                object targetObject = Activator.CreateInstance(objectType);
+
+                foreach (PropertyInfo property in objectType.GetProperties()
+                                                        .Where(p => p.CanRead && p.CanWrite))
+                {
+                    JsonPropertyAttribute attribute = property.GetCustomAttributes(true)
+                                                    .OfType<JsonPropertyAttribute>()
+                                                    .FirstOrDefault();
+
+                    string jsonPath = (attribute != null  ?attribute.PropertyName : property.Name);
+                    JToken token = null;
+                    if (!string.IsNullOrEmpty(jsonPath))
+                    {
+                        token = sourceObject.SelectToken(jsonPath);
+                        token ??= DecryptToken(sourceObject, jsonPath);
+                    }
+                    if (token != null && token.Type != JTokenType.Null)
+                    {
+                        object value = token.ToObject(property.PropertyType, serializer);
+                        property.SetValue(targetObject, value, null);
+                    }
+                   
+                }
+                if (targetObject != null)
+                    return targetObject;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentNullException(ex.Message);
+            }
+            return string.Empty;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return false;
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value,
+                                       JsonSerializer serializer)
+        {
+        }
+
+        private static JToken DecryptToken(JObject jObject, string path)
+        {
+            JToken jToken = null;
+            try
+            {
+                string firstElement = path.Split(".")[0];
+                if (path.Contains('0'))
+                {
+                    jToken = jObject[firstElement][0];
+                    path = path.Remove(0, path.IndexOf('0') + 2);
+                }
+                if ((jToken != null) && (path.Split(".").Any()))
+                {
+                    foreach (string property in path.Split("."))
+                    {
+                        jToken = jToken[property];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentNullException(ex.Message);
+            }
+            return jToken;
+        }
+
+    }
+}
